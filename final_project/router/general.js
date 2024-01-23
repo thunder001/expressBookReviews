c3543362fcc0d66ae5c1d18dcca93fd49d049de5
chunlei zheng <zhengcl75@gmail.com>
2024-01-22 20:13:44 -0500

    authentication

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -5,11 +5,32 @@ let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
 
-public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+//Function to check if the user exists
+const doesExist = (username)=>{
+  let userswithsamename = users.filter((user)=>{
+    return user.username === username
+  });
+  if(userswithsamename.length > 0){
+    return true;
+  } else {
+    return false;
+  }
+}
 
+public_users.post("/register", (req,res) => {
+  let username = req.query.username;
+  let password = req.query.password;
+  
+  if (username && password) {
+    if (!doesExist(username)) { 
+      users.push({"username":username,"password":password});
+      return res.status(200).json({message: "User successfully registred. Now you can login"});
+    } else {
+      return res.status(404).json({message: "User already exists!"});    
+    }
+  } 
+  return res.status(404).json({message: "Unable to register user."});
+})
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
     res.send(JSON.stringify({books}, null, 4));
@@ -54,8 +75,9 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    let isbn = req.params.isbn;
+    let review = books[isbn]["reviews"];
+    res.send(JSON.stringify(review));
 });
 
 module.exports.general = public_users;
